5445c972465f0588ef404ee021f4b54f590c3c4c
Warmor <grishin.pd@gmail.com>
2017-04-22 13:38:14 +0300

    Личный проект: подробности

--- js/pictures.js
+++ js/pictures.js
@@ -62,18 +62,19 @@ var generatePhotoObjects = function (settings) {
 
 var photosArray = generatePhotoObjects(photosSettings);
 
-var makeGalleryPicture = function (photo, template) {
+var makeGalleryPicture = function (photo, template, i) {
   var _photoElement = template.cloneNode(true);
   _photoElement.querySelector('img').src = photo.url;
   _photoElement.querySelector('.picture-likes').textContent = photo.likes;
   _photoElement.querySelector('.picture-comments').textContent = photo.comments.length;
+  _photoElement.querySelector('.picture').setAttribute('data-number', i);
   return _photoElement;
 };
 
 var makeFragment = function (photos, template) {
   var _fragment = document.createDocumentFragment();
   for (var i = 0; i < photos.length; i++) {
-    _fragment.appendChild(makeGalleryPicture(photos[i], template));
+    _fragment.appendChild(makeGalleryPicture(photos[i], template, i));
   }
   return _fragment;
 };
@@ -84,14 +85,107 @@ var fillGallery = function (photos) {
   document.querySelector('.pictures').appendChild(_fragment);
 };
 
-var makeMainPicture = function (photos) {
+var makeMainPicture = function (photos, number) {
   var _galleryOverlay = document.querySelector('.gallery-overlay');
-  _galleryOverlay.querySelector('img').src = photos[0].url;
-  _galleryOverlay.querySelector('.likes-count').textContent = photos[0].likes;
-  _galleryOverlay.querySelector('.comments-count').textContent = photos[0].comments.length;
-  _galleryOverlay.classList.remove('invisible');
+  _galleryOverlay.querySelector('img').src = photos[number].url;
+  _galleryOverlay.querySelector('.likes-count').textContent = photos[number].likes;
+  _galleryOverlay.querySelector('.comments-count').textContent = photos[number].comments.length;
 };
 
 fillGallery(photosArray);
-makeMainPicture(photosArray);
-document.querySelector('.upload-overlay').classList.add('invisible');
+
+// Показ/скрытие картинки в галерее
+
+var ESC_KEY_CODE = 27;
+var ENTER_KEY_CODE = 13;
+var picturesContainer = document.querySelector('.pictures');
+var galleryOverlay = document.querySelector('.gallery-overlay');
+var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+
+var isKeyPressed = function (evt, code) {
+  return evt.keyCode === code;
+};
+
+var onKeyPressed = function(evt, code, callback) {
+  if (isKeyPressed(evt, code)) {
+    callback();
+  }
+};
+
+var openGallery = function(number) {
+  makeMainPicture(photosArray, number);
+  document.addEventListener('keydown', onEscPressGalleryOverlay);
+  galleryOverlay.classList.remove('invisible');
+};
+
+var closeGallery = function(evt) {
+  document.removeEventListener('keydown', onEscPressGalleryOverlay);
+  galleryOverlay.classList.add('invisible');
+};
+
+var onEscPressGalleryOverlay = function (evt) {
+  onKeyPressed(evt, ESC_KEY_CODE, closeGallery);
+};
+var onClickGalleryOverlayClose = function() {
+  closeGallery();
+};
+var onKeydownGalleryOverlayClose = function(evt) {
+  onKeyPressed(evt, ENTER_KEY_CODE, closeGallery);
+};
+
+var onClickPicturesContainer = function(evt) {
+  evt.preventDefault();
+  var target = event.target;
+  var picture = target.closest('.picture');
+  if (!picture) { return;}
+  var number = picture.getAttribute('data-number');
+  openGallery(number);
+};
+picturesContainer.addEventListener('click', onClickPicturesContainer);
+galleryOverlayClose.addEventListener('click', onClickGalleryOverlayClose);
+galleryOverlayClose.addEventListener('keydown', onKeydownGalleryOverlayClose);
+
+// Показ/скрытие формы кадрирования
+
+var uploadForm = document.querySelector('#upload-select-image');
+var uploadFormFile = uploadForm.querySelector('#upload-file');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadOverlayDescription = uploadOverlay.querySelector('.upload-form-description');
+var uploadOverlayForm = uploadOverlay.querySelector('.upload-filter');
+var uploadOverlayCancel = uploadOverlay.querySelector('.upload-form-cancel');
+var uploadOverlaySubmit = uploadOverlay.querySelector('.upload-form-submit');
+
+var onEscPressUploadOverlay = function(evt) {
+  onKeyPressed(evt, ESC_KEY_CODE, closeUploadOverlay);
+};
+var openUploadOverlay = function() {
+  uploadForm.classList.add('invisible');
+  uploadOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', onEscPressUploadOverlay);
+};
+
+var closeUploadOverlay = function() {
+  uploadForm.classList.remove('invisible');
+  uploadOverlay.classList.add('invisible');
+  document.removeEventListener('keydown', onEscPressUploadOverlay);
+};
+
+var onChangeUploadFile = function() {
+  openUploadOverlay();
+};
+
+uploadFormFile.addEventListener('change', onChangeUploadFile);
+uploadOverlayCancel.addEventListener('click', function() {
+  closeUploadOverlay();
+});
+uploadOverlaySubmit.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  closeUploadOverlay();
+});
+
+uploadOverlayDescription.addEventListener('keydown', function(evt) {
+  if (isKeyPressed(evt, ESC_KEY_CODE)) {
+    evt.stopPropagation();
+  }
+});
+closeUploadOverlay();
